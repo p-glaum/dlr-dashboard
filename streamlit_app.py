2ed001b2caf6dea42af9ae4e32767d03d08956a2
Philipp Glaum <p.glaum@tu-berlin.de>
2023-08-14 10:57:16 +0200

    updated version

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,12 @@ import hvplot.pandas
 import pypsa
 import os
 import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
 
 from helpers import prepare_colors, rename_techs_tyndp, get_cmap
 
 CACHE_TTL = 24*3600  # seconds
+pd.options.plotting.backend = 'holoviews'
 
 
 @st.cache_data(persist=True)
@@ -42,10 +44,10 @@ def load_country_shape():
 @st.cache_data(persist=True)
 def open_networks():
     networks = dict()
-    for file in os.listdir("./data/"):
+    for file in os.listdir("./data/networks"):
         if not file.endswith(".nc"):
             continue
-        n = pypsa.Network("./data/" + file)
+        n = pypsa.Network("./data/networks/" + file)
         year = file.split("_")[0][2:]
         constraint = file.split("_")[3].split("-")[1]
         dlr = file.split("_")[4]
@@ -55,53 +57,30 @@ def open_networks():
     return networks
 
 
-def scale_bus_sizes(bus_sizes, bus_size_factor):
-    if bus_size_factor is None:
-        max_size_factor = 0.16
-        bus_size_factor = max_size_factor / \
-            bus_sizes.groupby("bus").sum().max()
-    return bus_size_factor
-
-
-def scale_line_widths(line_widths, line_width_factor):
-    if line_width_factor is None:
-        max_line_factor = 5
-        line_width_factor = max_line_factor / line_widths.max()
-    return line_width_factor
-
-
-def plot_capacity_map(ax, n, bounds, bus_size_factor=None, line_width_factor=None, with_colormap=False
-                      ):
-    # Plots capacity of all generators in MW, existing and newly built
-    g = n.generators.p_nom_opt
-    bus_sizes = g.groupby([n.generators.bus, n.generators.carrier]).sum()
-    bus_sizes.drop("load", errors="ignore", level=1, inplace=True)
-    bus_size_factor = scale_bus_sizes(bus_sizes, bus_size_factor)
-    link_widths = n.links.p_nom_opt
-    line_widths = n.lines.s_nom_opt
-    line_width_factor = scale_line_widths(line_widths, line_width_factor)
-    # color map lines
-    cmap = cm.get_cmap("viridis", 256)
-    line_colors = n.get_switchable_as_dense("Line", "s_max_pu").mean() / 0.7
-    norm = colors.Normalize(vmin=line_colors.min(), vmax=line_colors.max())
-
-    with plt.rc_context({"patch.linewidth": 0}):
-        collection = n.plot(
-            ax=ax,
-            line_widths=line_widths * line_width_factor,
-            link_widths=link_widths * line_width_factor,
-            line_colors=line_colors,
-            line_cmap=cmap,
-            bus_sizes=bus_sizes * bus_size_factor,
-            bus_alpha=0.7,
-            color_geomap=False,
-            boundaries=bounds,
-        )
-
-    if with_colormap:
-        collection[1].set(norm=norm)
-        plt.colorbar(collection[1], ax=ax, fraction=0.04,
-                     pad=0.004, label="DLR / SLR")
+def network_selector(scenario_selection, dlr_selection):
+    if scenario_selection == "year 2019":
+        year = "2020"
+        share = ""
+    elif scenario_selection == "80% renewable share":
+        year = "2030"
+        share = "0.8"
+    elif scenario_selection == "85% renewable share":
+        year = "2030"
+        share = "0.85"
+    elif scenario_selection == "90% renewable share":
+        year = "2030"
+        share = "0.9"
+    elif scenario_selection == "95% renewable share":
+        year = "2030"
+        share = "0.95"
+    elif scenario_selection == "100% renewable share":
+        year = "2030"
+        share = "1.0"
+    if dlr_selection == "unlimited":
+        dlr = ""
+    else:
+        dlr = dlr_selection/100
+    return f"{year}-\\w*{share}-dlr{dlr}-\\w*"
 
 
 # MAIN
@@ -142,15 +121,12 @@ with st.sidebar:
         "View", view_list, help="Choose your view on the system.")
 
     custom_settings = dict()
+    dlr_rating_options = [100, 130, 150, 180, 200, "unlimited"]
+    scenario_list = ["year 2019"] + \
+        [f"{x}% renewable share" for x in range(80, 105, 5)]
+    barplot_parameters = ["optimal capacities",
+                          "generation", "curtailment", "cost"]
 
-    dlr_rating_options = [100, 120, 140, 150]
-    custom_settings["dlr_rating"] = st.select_slider(
-        "DLR Rating", dlr_rating_options, help="Choose dynamic line rating factor.")
-
-    scenario_list = ["year 2019",
-                     "80% renewable share", "100 % renewable share"]
-    custom_settings["scenario"] = st.selectbox(
-        "Scenario", scenario_list, help="Choose scenario.")
 
 networks = pd.Series(open_networks())
 
@@ -158,70 +134,142 @@ if (display == "Map explorer"):
 
     st.title("Map explorer")
 
-    gdf = load_country_shape().drop("name", axis=1)
+    _, col1, col2, _ = st.columns([1, 50, 50, 1])
 
-    opts = dict(
-        active_tools=['pan', 'wheel_zoom']
-    )
-    crs = ccrs.Geodetic()
+    with col1:
+        custom_settings["dlr_rating"] = st.select_slider(
+            "DLR Rating", dlr_rating_options, help="Choose dynamic line rating factor.")
 
-    # plot german shapefile
-    plot = gdf.hvplot(
-        # geo=True,
-        height=720,
-        tiles=config["tiles"],
-        color='white',
-        alpha=0.7,
-    ).opts(**opts)
+    with col2:
+        custom_settings["scenario"] = st.selectbox(
+            "Scenario", scenario_list, help="Choose scenario.")
 
-    # plot lines
-    n = networks.iloc[0]
-    lines = n.lines
-    edges = lines.loc[:, ["bus0", "bus1"]]
-    attr = ["Total Capacity (GW)", "Length (km)"]
-    edges.loc[:, "Total Capacity (GW)"] = lines.s_nom_opt.clip(lower=1e-3)/1e3
-    edges.loc[:, "Length (km)"] = lines.length
-    pos = n.buses[["x", "y"]].apply(tuple, axis=1).to_dict()
-    G = nx.from_pandas_edgelist(edges, 'bus0', 'bus1', edge_attr=attr)
-    # line_plot = hvnx.draw(
-    #     G,
-    #     pos=pos,
-    #     # node_size=4,
-    #     # node_color='grey',
-    #     edge_color='Total Capacity (GW)',
-    #     inspection_policy="edges",
-    #     # edge_width=4,
-    #     crs=ccrs.Geodetic(),
-    # ).opts(**opts)
+    n = networks.filter(regex=network_selector(
+        custom_settings["scenario"], custom_settings["dlr_rating"]))
 
-    # plot *= line_plot
+    if n.empty:
+        st.error("No data available for this selection.")
+        st.stop()
+    else:
+        n = n.iloc[0]
 
-    # plot buses
-    buses = gpd.GeoDataFrame(index=n.buses.index, geometry=gpd.points_from_xy(
-        n.buses.x, n.buses.y), crs=crs)
-    bus_size = n.statistics.optimal_capacity(groupby=n.statistics.get_bus_and_carrier)[
-        "Generator"]
-    # buses = buses.merge(bus_size.unstack(), left_index=True, right_index=True)
-    node_plot = buses.hvplot(
-        # geo=True,
-        alpha=0.7,
-        # hover_cols=list(bus_size.index.get_level_values(1).unique()),
-    )
-    plot *= node_plot
+        county_shape = load_country_shape().drop("name", axis=1)
 
-    # wanted to show an additional plot when hovering over a bus showing the capacities of the bus
-    # def capacity_plot(index):
-    #     plot = bus_size[index].hvplot.bar()
-    #     return plot
-    # capacity_dmap = hv.DynamicMap(capacity_plot, streams=[
-    #                               hv.streams.Selection1D(source=node_plot)])
+        opts = dict(
+            active_tools=['wheel_zoom']
+        )
+        crs = "4326"
 
-    # plot *= capacity_dmap
+        # plot german shapefile
+        plot = county_shape.hvplot(
+            geo=True,
+            color='white',
+            alpha=0.7,
+            crs=crs,
+            width=400, height=300
+        ).opts(**opts)
 
-    st.bokeh_chart(hv.render(plot, backend='bokeh'),
-                   use_container_width=True)
+        # plot lines
+        lines = n.lines
+        edges = lines.loc[:, ["bus0", "bus1"]]
+        attr = ["Total Capacity (GW)", "Length (km)"]
+        edges.loc[:, "Total Capacity (GW)"] = lines.s_nom_opt.clip(
+            lower=1e-3)/1e3
+        edges.loc[:, "Length (km)"] = lines.length
+        pos = n.buses[["x", "y"]].apply(tuple, axis=1).to_dict()
+        G = nx.from_pandas_edgelist(edges, 'bus0', 'bus1', edge_attr=attr)
+        line_plot = hvnx.draw(
+            G,
+            pos=pos,
+            node_size=0,
+            edge_color='Total Capacity (GW)',
+            inspection_policy="edges",
+            # edge_width=4,
+            crs=crs,
+        )
+
+        plot *= line_plot
+
+        # plot buses
+        # buses = n.buses[["x", "y"]].reset_index()
+        bus_size = n.statistics.optimal_capacity(groupby=n.statistics.get_bus_and_carrier)[
+            "Generator"].drop("load", level=1, errors="ignore")
+        # # buses = buses.merge(bus_size.unstack(), left_index=True,
+        # #                     right_index=True).reset_index()
+        buses = gpd.GeoDataFrame(index=n.buses.index, geometry=gpd.points_from_xy(
+            n.buses.x, n.buses.y), crs=crs).reset_index()
+
+        # node_plot = hv.Scatter(buses, 'x', 'y').opts(
+        #     tools=['tap', 'hover'], size=10,  color="blue", alpha=0.7, hover_color="red")
+        node_plot = buses.hvplot().opts(
+            tools=['tap'], active_tools=['tap'], size=15)
+
+        plot *= node_plot
+
+        # plot the capacity as hv Barplot
+        def plot_capacity_bar(index):
+            print(index)
+            if not index:
+                return hv.Bars(pd.Series()).relabel("No selection")
+            else:
+                bus = buses.iloc[tap.index[0]]["Bus"]
+                bar_plot = hv.Bars(bus_size[bus].dropna())
+                bar_plot.opts(title=f"Bus {bus}", xlabel="Carrier",
+                              ylabel="Capacity (GW)", width=400, height=300)
+                return bar_plot
+
+        # Define a Stream object to handle the tap event
+        tap = hv.streams.Selection1D('Tap', source=node_plot)
+        tap.param.mode = 'single'
+        # %%
+        # Create a dynamic plot that updates when a point is clicked
+        dynamic_bar = hv.DynamicMap(
+            plot_capacity_bar, streams=[tap])
+
+        # Display the dynamic plot
+        layout = (plot + dynamic_bar).cols(2)
+
+        st.bokeh_chart(hv.render(layout, backend='bokeh'),
+                       use_container_width=True)
+
 
 if (display == "Bar plot explorer"):
+
+    st.title("Bar plot explorer")
+
+    custom_settings["barplot_parameters"] = st.selectbox(
+        "Parameter", barplot_parameters, help="Choose a parameter you want to plot as bar plot.")
+
+    _, col1, col2, _ = st.columns([1, 50, 50, 1])
+
+    with col1:
+        custom_settings["dlr_rating"] = st.select_slider(
+            "DLR Rating", dlr_rating_options, help="Choose dynamic line rating factor.")
+
+    with col2:
+        custom_settings["scenario"] = st.selectbox(
+            "Scenario", scenario_list, help="Choose scenario.")
+
+    n = networks.filter(regex=network_selector(
+        custom_settings["scenario"], custom_settings["dlr_rating"]))
+    if n.empty:
+        st.error("No data available for this selection.")
+        st.stop()
+    n = n.iloc[0]
+    if custom_settings["barplot_parameters"] == "optimal capacities":
+        plot = n.statistics.optimal_capacity().hvplot.bar()
+    elif custom_settings["barplot_parameters"] == "generation":
+        plot = n.statistics.dispatch().hvplot.bar()
+    elif custom_settings["barplot_parameters"] == "curtailment":
+        plot = n.statistics.curtailment().hvplot.bar()
+    elif custom_settings["barplot_parameters"] == "cost":
+        plot = pd.concat([n.statistics.capex(), n.statistics.opex()], axis=1, keys=[
+                         "capex", "opex"]).hvplot.bar()
+
+    st.bokeh_chart(hv.render(plot, backend='bokeh'), use_container_width=True)
+
+
+if (display == "Network comparison"):
 
     st.title("Bar plot explorer")
 
